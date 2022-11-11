042d9337fde8bfd6026313219aab619d8d7f79c8
Márton Iványi <marton.ivanyi@bcsconsult.hu>
2022-11-11 14:54:34 +0100

    c3

--- Entities/User.cs
+++ Entities/User.cs
@@ -1,14 +1,12 @@
-3.	public class User
-4.	{
-5.	    public Guid ID { get; set; } = Guid.NewGuid();
-6.	    public string FirstName { get; set; }
-7.	    public string LastName { get; set; }
-21.	    public string FullName
-22.	        => string.Format(
-23.	            "{0} {1}",
-24.	            LastName,
-25.	            FirstName);
-26.	    
-27.	}
-18.		    
-
+public class User
+	{
+	    public Guid ID { get; set; } = Guid.NewGuid();
+	    public string FirstName { get; set; }
+	    public string LastName { get; set; }
+	    public string FullName
+	        => string.Format(
+	            "{0} {1}",
+            LastName,
+	            FirstName);
+    
+}
