545b57502b85aef3451d08a45e8ceeff6a391009
Márton Iványi <marton.ivanyi@bcsconsult.hu>
2022-11-11 14:48:10 +0100

    commit 2

--- Entities/User.cs
+++ Entities/User.cs
@@ -3,23 +3,12 @@
 5.	    public Guid ID { get; set; } = Guid.NewGuid();
 6.	    public string FirstName { get; set; }
 7.	    public string LastName { get; set; }
-8.	    public string FullName
-9.	    {
-10.	        get
-11.	        {
-12.	            return string.Format(
-13.	                "{0} {1}", 
-14.	                LastName,
-15.	                FirstName);
-16.	        }
-17.	    }
-18.	
-19.	    // Ugyanaz a FullName property kompaktabb formában is írható
-20.	    /*
 21.	    public string FullName
 22.	        => string.Format(
 23.	            "{0} {1}",
 24.	            LastName,
 25.	            FirstName);
-26.	    */
+26.	    
 27.	}
+18.		    
+
